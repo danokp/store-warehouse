c755ea94409569a2f2fd6a354e153201c8e21ee5
Daniil <danokp@mail.ru>
2023-07-21 15:27:30 +0400

    Add orders application

--- Makefile
+++ Makefile
@@ -2,3 +2,10 @@ MANAGE := poetry run python3 manage.py
 
 runserver:
 	@$(MANAGE) runserver
+
+makemigrations:
+	@$(MANAGE) makemigrations
+
+migrate: makemigrations
+	@$(MANAGE) migrate
+
---
+++ store_warehouse/orders/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class Order(models.Model):
+    order_number = models.CharField(max_length=255)
+
+    STATUS_CHOICES = (
+        ('new', 'New'),
+        ('in progress', 'In progress'),
+        ('stored', 'Stored'),
+        ('send', 'Send'),
+    )
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES)
+
+
+    def __str__(self):
+        return f'{self.order_number}-{self.status}'
