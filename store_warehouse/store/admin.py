e4000842926df844c5435e45c0078663fc0ca89c
Daniil <danokp@mail.ru>
2023-07-21 21:04:35 +0400

    Add store application

---
+++ store_warehouse/store/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from store_warehouse.store.models import StoreOrder
+
+
+admin.site.register(StoreOrder)
---
+++ store_warehouse/store/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+from store_warehouse.orders.models import Order
+
+
+class StoreOrder(Order):
+    pass
