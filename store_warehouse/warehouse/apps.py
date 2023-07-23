723ec6513b1a6e668485dd14928aefcc4600ff40
Daniil <danokp@mail.ru>
2023-07-23 19:04:55 +0400

    Connect signals to applications

--- store_warehouse/warehouse/apps.py
+++ store_warehouse/warehouse/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WarehouseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store_warehouse.warehouse'
+
+    def ready(self):
+        from . import signals
