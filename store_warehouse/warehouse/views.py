c37bc1720ae008a4be623c4be40456e17259b48a
Daniil <danokp@mail.ru>
2023-07-24 18:47:02 +0400

    Update perform_create method in Warehouse views.py

--- store_warehouse/warehouse/views.py
+++ store_warehouse/warehouse/views.py
@@ -2,8 +2,17 @@ from .models import WarehouseOrder
 from store_warehouse.warehouse.serializers import WarehouseOrderSerializer
 from store_warehouse.orders.views import OrderAPIViewSet
 
+from store_warehouse.orders.models import SynchConnection
+
 
 class WarehouseOrderAPIViewSet(OrderAPIViewSet):
     basename = 'warehouseorder'
     queryset = WarehouseOrder.objects.all()
     serializer_class = WarehouseOrderSerializer
+
+    def perform_create(self, serializer):
+        '''With use of received url identify store that created order'''
+        serializer.validated_data['connection'] = SynchConnection.objects.get(
+            url=serializer.validated_data.get('connection')
+        )
+        serializer.save()
