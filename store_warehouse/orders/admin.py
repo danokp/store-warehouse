cdf7c21b94b445987dad90be6f74bf71ea63d3c7
Daniil <danokp@mail.ru>
2023-07-24 11:59:46 +0400

    Add SynchConnection model

--- store_warehouse/orders/admin.py
+++ store_warehouse/orders/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
+from .models import SynchConnection
 
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
+admin.site.register(SynchConnection)
--- store_warehouse/orders/models.py
+++ store_warehouse/orders/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 
+class SynchConnection(models.Model):
+    '''Model for connection to another client using token.'''
+
+    name = models.CharField(max_length=255, unique=True)
+    link = models.URLField(max_length=100, unique=True)
+    token = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Order(models.Model):
+    '''Abstract model used for creation StoreOrder and WarehouseOrder models'''
+
     order_number = models.CharField(max_length=255, unique=True)
 
     STATUS_CHOICES = (
@@ -10,7 +23,7 @@ class Order(models.Model):
         ('stored', 'Stored'),
         ('send', 'Send'),
     )
-    status = models.CharField(max_length=255, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES)
     prev_status = models.CharField(
         max_length=255,
         choices=STATUS_CHOICES,
@@ -18,8 +31,10 @@ class Order(models.Model):
         null=True,
     )
 
+    connection = models.ForeignKey(SynchConnection, on_delete=models.PROTECT)
+
     def __str__(self):
-        return f'order: {self.order_number}-{self.status}'
+        return f'order: {self.order_number}-{self.connection}-{self.status}'
 
     class Meta:
         abstract = True
