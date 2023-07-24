54abe9b64aba9cbbc2a88c3fe480ef6a0183a4f9
Daniil <danokp@mail.ru>
2023-07-24 21:14:52 +0400

    Give access only for authenticated users

--- store_warehouse/orders/views.py
+++ store_warehouse/orders/views.py
@@ -1,5 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 
 
-class OrderAPIViewSet(viewsets.ModelViewSet):
-    pass
+class OrderAPIViewSet(mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, )
